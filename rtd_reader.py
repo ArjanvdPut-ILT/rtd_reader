ed40d5a86453999e26e95dee1ca10b03e561cb59
AvdP <arjan.vander.put@ilent.nl>
2018-05-18 13:27:45 +0200

    * Removed:   * example rtd file   * RTD_table class   * id __name__=='__main__' code * Added:   * xml parsing methods   * imports   * list_geo_xml_colums() method in class RTD

--- rtd_reader.py
+++ rtd_reader.py
@@ -1,9 +1,10 @@
 import sys
+import os.path
 from sqlalchemy import create_engine
 from lxml import etree
 import pandas as pd
-import os.path
-
+import geopandas as gpd
+from shapely.geometry import Point, MultiPoint, LineString, MultiLineString, Polygon, MultiPolygon
 
 class RTD(object):
     """A RingToets SQLite database
@@ -11,16 +12,13 @@ class RTD(object):
     <More elaborate doc>
     """
 
-    def __init__(self, rtd_file=None):
+    def __init__(self, rtd_file):
 
         """
 
         :type rtd_file: path to .rtd file
         """
         self.rtd_file = rtd_file
-        if self.rtd_file == None:
-            self.rtd_file = 'data/example2.rtd'
-
         self.engine = create_engine('sqlite:///{}'.format(self.rtd_file))
 
     def __repr__(self):
@@ -60,103 +58,80 @@ class RTD(object):
             result = connection.execute('SELECT * FROM {}'.format(table_name))
             return result.keys()
 
+    def list_geo_xml_columns(self, table_name):
+        """List all columns in a table that contain geometry in xml
+
+        """
+        return [col for col in self.list_columns(table_name) if col.lower().endswith('xml')]
+
     def table_to_df(self, tbl):
         """SQLite table to pandas dataframe
     
         """
-        #return pd.read_sql(tbl, self.engine)
         return pd.read_sql('SELECT * FROM {}'.format(tbl), self.engine)
 
+    def table_to_geodf(self, table_name, geotype='point'):
+        """Return a table as geopandas GeoDataframe
 
-class RTD_table(object):
-    """A table from a Ringtoets database as a pandas dataframe
-    
+        """
+        gdf = gpd.GeoDataFrame(self.table_to_df(table_name))
+
+        for col in self.list_geo_xml_columns(table_name):
+            geo_column_name = col +'_geo'
+            gdf[geo_column_name] = gdf[col].apply(xml_to_geometry, geotype=geotype)
+
+        gdf.set_geometry(geo_column_name, inplace=True)
+
+        return gdf
+
+def xml_str_to_tree(xml_str):
+    """Return etree for xml_str
     """
 
-    def __init__(self, RTD_obj, table_name):
-        """
-        
-        :param RTD_obj: 
-        :param table_name: 
-        """
-        self.RTD = RTD_obj
-        self.table_name = table_name
+    return etree.fromstring(xml_str)
 
-    def __repr__(self):
-        return '{} instance of table "{}" in {}'.format(self.__class__,
-                                                        self.table_name,
-                                                        self.RTD)
 
-    def info(self):
-        """Information on shape and contents of the table
-        
-        :return:
-        """
-        # TODO: check if using pandas info method is a better solution
-        info = ""
-        info += "{:65} #-Shape: {:10}    #-Bytes: {:_>6}\n".format(self.table_name,
-                                                                   str(self.as_df().shape),
-                                                                   sys.getsizeof(self.as_df()))
+def get_xyz_from_xml_as_dict(tree):
+    """Find x, y, z tags in xml and return a dict of their values
 
-        for col in self.as_df().columns:
-            info += "    {:69} #-dtype: {:6}\n".format(col, self.as_df()[col].dtype)
+    :return:
+    """
+    data = {'x': [], 'y': [], 'z': []}
+    for element in tree.iter():
 
-        return info
+        if element.tag.split('}')[1] in ('x', 'y', 'z'):
+            # tag = element.getparent().tag
+            data[element.tag.split("}")[1]].append(float(element.text))
 
-    def as_df(self):
-        """SQLite table as pandas dataframe
+    # Remove empty keys
+    for key in data.keys():
+        if len(data[key]) == 0:
+            data.pop(key, None)
+    return data
 
-        """
 
-        return pd.read_sql('SELECT * FROM {}'.format(self.table_name), self.RTD.engine)
+def xml_to_geometry(xml_str, geotype=None):
+    """Turn xml string into shapely geometry
 
-        # Statement below does not work in older pandas versions
-        #return pd.read_sql_query('SELECT * FROM {}'.format(self.table_name), self.RTD.engine)
+    """
 
-    @property
-    def xml_columns(self):
-        """Check which columns contain xml
+    tree = xml_str_to_tree(xml_str)
+    xyz_dict = get_xyz_from_xml_as_dict(tree)
+    coords = zip(xyz_dict['x'], xyz_dict['y'])
+    # TODO: determine geometrytype (point, line, polygon) and act accordingly
+    # TODO: z values
 
-        :return:
-        """
-        for col in self.as_df().columns:
-            if 'xml' in str(col).lower():
-                yield col
+    if geotype == 'line':
+        geometry = LineString(coords)
+    elif geotype == 'point':
+        geometry = MultiPoint(coords)
+    elif geotype == 'polygon':
+        geometry = Polygon(coords)
+    else:
+        return None
 
-    def xml_str_to_tree(self, xml_str):
-        """Return etree for xml_str
-        """
-
-        # http://lxml.de/tutorial.html#the-fromstring-function
-        return etree.fromstring(xml_str)
-
-    def get_xyz_from_xml_as_dict(self, tree):
-        """Find x, y, z tags in xml and return a dict of their values
-
-        :return:
-        """
-        data = {'x': [], 'y': [], 'z': []}
-        for element in tree.iter():
-
-            if element.tag.split('}')[1] in ('x', 'y', 'z'):
-                #tag = element.getparent().tag
-                data[element.tag.split("}")[1]].append(float(element.text))
-
-        # Remove empty keys
-        for key in data.keys():
-            if len(data[key]) == 0:
-                data.pop(key, None)
-        return data
+    return geometry
 
 
 if __name__ == '__main__':
-    rtd = RTD()
-    for tbl in rtd.list_table_names():
-        t = RTD_table(rtd,tbl)
-        print(t.info())
-        for xml_col in t.xml_columns:
-            #print 'xml_col: {}'.format(xml_col)
-            for xml_str in t.as_df()[xml_col]:
-                # print(xml_str[:50])
-                tree = t.xml_str_to_tree(xml_str)
-                #print((t.get_xyz_from_xml_as_dict(tree).keys()))
+   pass
